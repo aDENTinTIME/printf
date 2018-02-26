94be15c772b58a0fd26ac9bafedd6e30a4ef858e
Lizzie Turner <hello@lizzieturner.com>
2018-02-26 20:56:49 +0000

    Merge from mandatory

---
+++ _printf.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * _printf - Prints a formatted string, similar to printf.
+ * @format: Arguments passed to program.
+ *
+ * Return: Returns length of string.
+ */
+
+int _printf(const char *format, ...)
+{
+	int f_i, s_i;
+	va_list args;
+	int count = 0;
+	int (*func)(va_list);
+	char *current;
+
+	va_start(args, format);
+
+	for (f_i = 0; format != NULL && format[f_i] != '\0'; f_i++)
+	{
+		if (format[f_i] == '%')
+		{
+			f_i++;
+			func = get_spec_func(format[f_i]);
+			count += func(args);
+		}
+		else
+		{
+			_putchar(format[f_i]);
+			count++;
+		}
+	}
+
+	va_end(args);
+
+	return (count);
+}
