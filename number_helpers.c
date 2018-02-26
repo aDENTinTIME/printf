0c43d56ee074f6817812d9b54b3d16d4a535bba8
Lizzie Turner <hello@lizzieturner.com>
2018-02-26 04:36:23 +0000

    Create print_decimal and print_integer functions in number_helpers.c

--- number_helpers.c
+++ number_helpers.c
@@ -10,6 +10,36 @@
 int print_decimal(va_list d)
 {
 	int count = 0;
+	int digit, sum, last;
+	int divis = 1000000000;
+	int num;
+
+	num = va_arg(d, int);
+	sum = 0;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num *= -1;
+		count++;
+	}
+
+	while (divis > 1)
+	{
+		digit = (num / divis) % 10;
+		sum += digit;
+		if (sum != 0)
+		{
+			_putchar(digit + '0');
+			count++;
+		}
+		divis = divis / 10;
+	}
+
+	last = num % 10;
+	_putchar(last + '0');
+	count++;
+
 	return (count);
 }
 
@@ -23,5 +53,34 @@ int print_decimal(va_list d)
 int print_integer(va_list i)
 {
 	int count = 0;
+	int digit, sum, last;
+	int divis = 1000000000;
+	int num;
+
+	num = va_arg(i, int);
+	sum = 0;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num *= -1;
+		count++;
+	}
+
+	while (divis > 1)
+	{
+		digit = (num / divis) % 10;
+		sum += digit;
+		if (sum != 0)
+		{
+			_putchar(digit + '0');
+			count++;
+		}
+		divis = divis / 10;
+	}
+
+	last = num % 10;
+	_putchar(last + '0');
+	count++;
 	return (count);
 }
