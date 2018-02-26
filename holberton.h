44f5ed69ab4951fd9b90a0a7928216f744f7bf56
adentintime <arixbizz@gmail.com>
2018-02-26 01:22:31 +0000

    Added unistd.h and changed return of void to int

--- holberton.h
+++ holberton.h
@@ -4,9 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 /**
-* struct format - Struct format_t.
+* struct format - Struct spec_t.
 * @sym: symbol.
 * @f: function.
 */
@@ -14,15 +15,15 @@
 typedef struct format
 {
 	char *sym;
-	void (*f)(va_list);
+	int (*f)(va_list);
 } spec_t;
 
 
 int _putchar(char c);
 
-void print_char(va_list c);
-void print_int(va_list i);
-void print_decimal(va_list d);
-void print_string(va_list s);
+int print_char(va_list c);
+int print_int(va_list i);
+int print_decimal(va_list d);
+int print_string(va_list s);
 
 #endif
