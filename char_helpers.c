234fc7c55593400e09525978181a33fecfeaa539
Lizzie Turner <hello@lizzieturner.com>
2018-02-26 20:59:58 +0000

    Fix betty errors in char_helpers.c

---
+++ char_helpers.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_char - prints argument of type char
+ * @c: argument to print
+ *
+ * Return: number of characters printed
+ * in this case, return value will always be 1
+ */
+
+int print_char(va_list c)
+{
+	char print_this = va_arg(c, int);
+
+	_putchar(print_this);
+
+	return (1);
+}
+
+
+/**
+ * print_percent - prints percent sign
+ * @p: percent sign
+ *
+ * Return: number of character printed
+ * in this case, return value will always be 1
+ */
+int print_percent(va_list p)
+{
+	_putchar('%');
+
+	return (1);
+}
