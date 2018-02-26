7c97cf6039bd88a75ded049b5c6afe7c8134a16d
Lizzie Turner <hello@lizzieturner.com>
2018-02-26 02:27:31 +0000

    Add signifier prototypes to holberton.h

--- holberton.h
+++ holberton.h
@@ -22,8 +22,18 @@ typedef struct format
 int _putchar(char c);
 
 int print_char(va_list c);
+int print_percent(va_list p);
 int print_int(va_list i);
 int print_decimal(va_list d);
 int print_string(va_list s);
+int print_special_string(va_list s);
+int print_rot13(va_list r);
+int print_reverse(va_list r);
+int print_pointer(va_list p);
+int print_binary(va_list b);
+int print_unsigned(va_list u);
+int print_octal(va_list o);
+int print_lil_hex(va_list x);
+int print_big_hex(va_list x);
 
 #endif
