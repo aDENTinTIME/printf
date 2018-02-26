47a9546ae8b69d2238ec0b9bfcdd4ad7b1f63e7c
adentintime <arixbizz@gmail.com>
2018-02-26 02:27:41 +0000

    Create number_helpers.c and translation_helpers.c

---
+++ number_helpers.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_decimal - prints argument of type decimal.
+ * @d: argument to print.
+ *
+ * Return: number of characters printed.
+ */
+
+int print_decimal(va_list d)
+{
+
+	return (count);
+}
+
+/**
+ * print_integer - prints argument of type integer.
+ * @i: argument to print.
+ *
+ * Return: number of characters printed.
+ */
+
+int print_integer(va_list i)
+{
+
+	return (count);
+}
---
+++ translation_helpers.c
@@ -0,0 +1,70 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints argument of type int in
+ * binary (base 2).
+ * @b: argument to print.
+ *
+ * Return: number of characters printed.
+ */
+
+int print_binary(va_list b)
+{
+
+	return (count);
+}
+
+/**
+ * print_unsigned - prints argument of type unsigned int.
+ * @u: argument to print.
+ *
+ * Return: number of characters printed.
+ */
+
+int print_unsigned(va_list u)
+{
+
+	return (count);
+}
+
+/**
+ * print_octal - prints argument of type int in
+ * octal (base 8).
+ * @o: argument to print.
+ *
+ * Return: number of characters printed.
+ */
+
+int print_octal(va_list o)
+{
+
+	return (count);
+}
+
+/**
+ * print_lil_hex - prints argument of type unsigned int in
+ * hex (base 16) using lowercase.
+ * @x: argument to print.
+ *
+ * Return: number of characters printed.
+ */
+
+int print_lil_hex(va_list x)
+{
+
+	return (count);
+}
+
+/**
+ * print_lil_hex - prints argument of type unsigned int in
+ * hex (base 16) using UPPERCASE.
+ * @x: argument to print.
+ *
+ * Return: number of characters printed.
+ */
+
+int print_big_hex(va_list x)
+{
+
+	return (count);
+}
